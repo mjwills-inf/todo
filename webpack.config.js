a9aad541afb083c2c8b91d2876218c73a68a3321
mjwills-inf <mjwills@live.co.uk>
2020-02-19 12:10:59 +0000

    file set up complete

--- webpack.config.js
+++ webpack.config.js
@@ -16,14 +16,7 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
+      },        
     ]
   }
 };
