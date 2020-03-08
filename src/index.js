4308e810eb1102de7128a35243bc1f72f40130a0
mjwills-inf <mjwills@live.co.uk>
2020-03-08 16:22:49 +0000

    problem with default rearrange, compare to index calls from before

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Render from './modules/render';
 import DefaultData from './modules/defaultData';
 
 let defaultData = DefaultData();
+defaultData.storageCheck(); // its run here (otherwise index fine)
 let projectContainer = defaultData.projectContainer;
 let defaultProject = defaultData.defaultProject;
 
